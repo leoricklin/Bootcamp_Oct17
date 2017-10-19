0afa0e77373a28956bf2c3f6213819982a566b2d
David Springate <david.springate@teradata.com>
2017-10-19 16:19:22 -0600

    revised plot script

--- use_case1/plot_crime_data.R
+++ use_case1/plot_crime_data.R
@@ -9,17 +9,23 @@ qog_df <- read.csv("outputs/qog_homicide_filtered.csv", stringsAsFactors = FALSE
 
 
 qog_reduced <- qog_df %>% 
-  select(country, continent, year, homicides, unna_pop, wdi_mortinf, unna_gdp,
-         wdi_lrmd, wdi_popurb, wdi_acis) %>%
+  select(country, continent, year, homicides, unna_pop, wdi_mortinf, unna_gdppc,
+         wdi_popurb, wdi_acisr, wdi_aciwr, unna_hce,
+         gea_ea1524m, gea_ea5564m, wdi_tele) %>%
    rename(pop_size = unna_pop,
          infant_mortality = wdi_mortinf,
-         GDP = unna_gdp,
-         maternal_death_risk = wdi_lrmd,
+         GDP_per_capita = unna_gdppc,
+         education_male_15_24 = gea_ea1524m,
+         education_male_55_64 = gea_ea5564m,
+         household_consumption = unna_hce,
          urban_pop_percent = wdi_popurb,
-         access_sanitation_percent = wdi_acis) %>% 
-  mutate(GDP_per_capita = GDP / pop_size)
-
+         rural_sanitation_access = wdi_acisr,
+         rural_water_access = wdi_aciwr,
+         telephone_subscriptions = wdi_tele)
+  
 write.csv(qog_reduced, file = "outputs/qog_reduced.csv", row.names = FALSE)
+write.csv(qog_reduced, file = "../Bootcamp_Oct17/dashboard/data/qog_reduced.csv", row.names = FALSE)
+
 
 p <- gvisMotionChart(qog_reduced, 
                      idvar="country", 
